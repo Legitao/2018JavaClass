5227c23a3cd09b6cc9002cdafa03e9b93885bfbe
Tao Zhu <zhutaosx@outlook.com>
2018-02-06 22:35:10 -0600

    2nd commit

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -4,7 +4,7 @@ public class TemperatureConverter {
 	
 	public static double celsisusToFahrenheit(double celsius) {
 		
-		double fahrenheit = (9.0/5.0)*celsius + 32;
+		double fahrenheit = (9.0/5.0)*celsius + 32; /**/
 		return fahrenheit;
 	}
 	
