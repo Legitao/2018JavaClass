a84c67aad36c1e8daed0ee24491695baea47b1a7
Tao Zhu <sjtuzt1997@sjtu.edu.cn>
2018-04-30 13:52:07 -0500

    ass7

--- assignment/7/assignment7/src/assignment7/Cheaters.java
+++ assignment/7/assignment7/src/assignment7/Cheaters.java
@@ -1,20 +1,33 @@
 package assignment7;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+/**
+ * 
+ * This class read documents under a folder and computes duplicate n-grams between each document
+ * pairs and output documents with high similarities.
+ *
+ */
 public class Cheaters {
 	public static Map<String, List<String>> map = new HashMap<>();
-	static Map<String, Map<String, Integer>> common = new HashMap<String, Map<String, Integer>>();
-	
+	static Map<String, Map<String, Integer>> common = new HashMap<>();
+
 	public static void main(String[] args) throws FileNotFoundException {
+		if(args.length != 3) {
+			System.out.println("Wrong commandline arguments");
+			return;
+		}
 		File folder = new File(args[0]);
 		File[] listOfFiles = folder.listFiles();
 		for(int i = 0; i < listOfFiles.length; i++) {
@@ -23,24 +36,30 @@ public class Cheaters {
 				common.get(listOfFiles[i].getName()).put(listOfFiles[j].getName(), 0);
 			}
 		}
+		//System.out.println(common.size());
 		for (File file : listOfFiles) {
 		    if(file.isFile()) {
-		    	Scanner input = new Scanner(file, "UTF-8");
+		    	Scanner input = new Scanner(new BufferedReader(new FileReader(file)));
+		    	//Scanner input = new Scanner(file, "UTF-8");
 		    	//System.out.println(file.getName());
 		    	List<String> words = new ArrayList<>();
 		    	while(input.hasNext()) {
-		    		String[] tmp = input.next().replaceAll("[^a-zA-Z0-9 ]", "").toLowerCase().split("\\s+");		    		
+		    		String[] tmp = input.next().replaceAll("[^a-zA-Z0-9]", "").toLowerCase().split("\\s+");		    		
 		    		words.addAll(Arrays.asList(tmp));
 		    	}
-		    	for(int i = 0; i < words.size() - 5; i++) {
-		    		String six_word = words.get(i) + " " + words.get(i + 1) + " "
-		    				+ words.get(i + 2) + " " + words.get(i + 3) + " "
-		    				+ words.get(i + 4) + " " + words.get(i + 5);
+		    	int n = Integer.valueOf(args[1]);
+		    	for(int i = 0; i < words.size() - n + 1; i++) {
+		    		String six_word = null;
+		    		for(int j = 0; j < n; j++) {
+		    			six_word += words.get(i + j);
+		    		}
+		    		
 		    		//System.out.println(six_word);
 		    		if(map.containsKey(six_word)) {
 		    			map.get(six_word).add(file.getName());
 		    		} else {
 		    			map.put(six_word, new ArrayList<String>());
+		    			map.get(six_word).add(file.getName());
 		    		}
 		    	}
 		    	input.close();
@@ -62,20 +81,44 @@ public class Cheaters {
 		    	}
 		    }
 		}
+		
+		List<Pair> result = new ArrayList<>();
 		for(int i = 0; i < listOfFiles.length; i++) {
 			for(int j = i + 1; j < listOfFiles.length; j++) {
 				int num = common.get(listOfFiles[i].getName()).get(listOfFiles[j].getName());
-				if(num > 0) {
-					System.out.print(listOfFiles[i].getName() + " " + listOfFiles[j].getName() + " ");
-					System.out.println(num);
-				}
-				
-				
-				
+				if(num > Integer.valueOf(args[2])) {
+					result.add(new Pair(listOfFiles[i].getName(), listOfFiles[j].getName(), num));
+					//System.out.print(listOfFiles[i].getName() + " " + listOfFiles[j].getName() + " ");
+					//System.out.println(num);
+				}				
 			}
 		}
-		System.out.println(args[1]);
-		System.out.println(args[2]);
+		Collections.sort(result);
+		Collections.reverse(result);
+		for(Pair e : result) {
+			System.out.print(e.a + " " + e.b + " " + e.num + "\n");
+		}
 	}
+}
 
+/**
+ * 
+ * This class is to represent a pair of similar documents. It has data fields of
+ * two documents names and the number of duplicate n-grams.
+ *
+ */
+class Pair implements Comparable<Pair>{
+	String a;
+	String b;
+	int num;
+	public Pair(String a, String b, int num) {
+		this.a = a;
+		this.b = b;
+		this.num = num;
+	}
+	@Override
+	public int compareTo(Pair o) {		
+		return this.num - o.num;
+	}
+	
 }
